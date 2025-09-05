0305868658d89837b7f6e2e4e0da4609532d674c
DefenestratorNZ <45814664+DefenestratorNZ@users.noreply.github.com>
2025-09-05 21:37:42 +1200

    Macros and martix

---
+++ keyboards/crgscoreboard/Macros.h
@@ -0,0 +1,28 @@
+{
+    "macros": {
+        "0": [
+            {
+                "action": 2,
+                "argument": "KC_LALT"
+            },
+            {
+                "action": 4,
+                "argument": "KC_P1"
+            },
+            {
+                "action": 4,
+                "argument": "KC_P8"
+            },
+            {
+                "action": 4,
+                "argument": "KC_P0"
+            },
+            {
+                "action": 3,
+                "argument": "KC_LALT"
+            }
+        ]
+        
+    },
+
+}
